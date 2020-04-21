f723a94f08c77ff4d1941413b1a9881fb4d801ee
Jon Flores <61673020+jonflores1093@users.noreply.github.com>
2020-04-21 02:48:28 -0500

    Project Forward
    Project

---
+++ Assets/Scripts/RandomGenObj.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomGenObj : MonoBehaviour
+{
+
+
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        GameObject.FindGameObjectsWithTag("Structure");
+        
+            
+
+        Vector3 randScale = new Vector3(Random.Range(0.0f, 10.0f), Random.Range(0.0f, 10.0f), Random.Range(0.0f, 10.0f));
+        transform.localScale = randScale;
+
+
+
+        
+
+
+    }
+    
+}
---
+++ Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+    
+
+    void OnTriggerEnter(Collider other)
+    {
+        Scene sceneCurrent = SceneManager.GetActiveScene();
+
+        string sceneName = sceneCurrent.name;
+
+        if (sceneName == "Street")
+        {
+            SceneManager.LoadScene("MushroomStreet");
+        }
+
+        else if (sceneName == "MushroomStreet")
+        {
+            SceneManager.LoadScene("DistortStreet");
+        }
+        else if (sceneName == "DistortStreet")
+        {
+            SceneManager.LoadScene("Menu");
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+    }
+}
