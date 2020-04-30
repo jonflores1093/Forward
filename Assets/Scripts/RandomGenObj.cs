3bcc5386d141473b0d8fa7e5557a015b575b8553
Jon Flores <61673020+jonflores1093@users.noreply.github.com>
2020-04-30 14:28:02 -0500

    Fixed Player Controller, Fixed maps, Fixed scene transitions

--- Assets/Scripts/RandomGenObj.cs
+++ Assets/Scripts/RandomGenObj.cs
@@ -5,21 +5,28 @@ using UnityEngine;
 public class RandomGenObj : MonoBehaviour
 {
 
-
+    public GameObject[] structures;
 
     void OnTriggerEnter(Collider other)
     {
-
-        GameObject.FindGameObjectsWithTag("Structure");
         
-            
-
-        Vector3 randScale = new Vector3(Random.Range(0.0f, 10.0f), Random.Range(0.0f, 10.0f), Random.Range(0.0f, 10.0f));
-        transform.localScale = randScale;
 
 
 
-        
+
+
+        structures = GameObject.FindGameObjectsWithTag("Structure");
+
+        foreach (GameObject structure in structures)
+        {
+
+            Vector3 randScale = new Vector3(Random.Range(0.0f, 10.0f), Random.Range(0.0f, 10.0f), Random.Range(0.0f, 10.0f));
+
+            structure.transform.localScale = randScale;
+
+
+
+        }
 
 
     }
